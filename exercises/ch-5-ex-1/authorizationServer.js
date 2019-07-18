06b62754bffaffccd0834e98e30eb0f858c5d2b5
shingo sasaki <shingo.sasaki.0529@gmail.com>
2019-07-19 00:52:55 +0900

    認可コードを取得するとこまで実装

--- exercises/ch-5-ex-1/authorizationServer.js
+++ exercises/ch-5-ex-1/authorizationServer.js
@@ -24,40 +24,99 @@ var authServer = {
   tokenEndpoint: 'http://localhost:9001/token'
 };
 
-// client information
+/**
+ * OAuthクライアントの一覧を管理する
+ * 実際は動的生成しながらRDBなどで永続化する
+ */
 var clients = [
-
-  /*
-   * Enter client information here
-   */
+  {
+    "client_id": "oauth-client-1",
+    "client_secret": "oauth-client-secret1",
+    "redirect_urls": ["http://localhost:9000/callback"]
+  }
 ];
 
+/**
+ * 認可コードの一覧
+ * 実際はRDBなどで永続化
+ */
 var codes = {};
 
+/**
+ * 認可エンドポイントへのリクエストをステートレスにするための変数
+ * 実際はRDBだどでセッション管理する
+ */
 var requests = {};
 
+/**
+ * クライアントIDをもとにクライアントを取得する
+ * 実際はRDSなどに対してクエリを発行するやつ
+ */
 var getClient = function(clientId) {
   return __.find(clients, function(client) { return client.client_id == clientId; });
 };
 
-app.get('/', function(req, res) {
-  res.render('index', {clients: clients, authServer: authServer});
-});
-
+/**
+ * 認可エンドポイント
+ * フロントチャネルで利用
+ */
 app.get("/authorize", function(req, res){
+  var client = getClient(req.query.client_id);
 
-  /*
-   * Process the request, validate the client, and send the user to the approval page
-   */
+  // 未知のクライアントだった場合、エラーページに誘導
+  if (!client) {
+    res.render('error', { error: 'お前クライアントのユーザちゃうやろ' });
+    return;
+  }
 
+  // 認可のためのセッション管理
+  var reqid = randomstring.generate(8);
+  requests[reqid] = req.query
+  console.log(requests)
+
+  // ユーザに権限委譲の承認をさせるための画面を描画
+  res.render('approve', { client: client, reqid: reqid });
 });
 
+/**
+ * 権限委譲の内容を伝えるエンドポイント
+ * フロントチャネルで利用
+ */
 app.post('/approve', function(req, res) {
+  var redirect_url;
+  var reqid = req.body.reqid;
+  var query = requests[reqid];
+  delete requests[reqid];
 
-  /*
-   * Process the results of the approval page, authorize the client
-   */
+  // CSRF対策
+  if (!query) {
+    res.render('error', { error: 'お前本人ちゃうやろ' });
+    return;
+  }
 
+  // ユーザが権限委譲を承認した場合
+  if (req.body.approve) {
+    // 認可コードによる付与方式が要求された場合
+    // クライアントに認可コードを伝えるためにリダイレクト
+    if (query.response_type == 'code') {
+      var code = randomstring.generate(8);
+      codes[code] = { request: query };
+      redirect_url = buildUrl(query.redirect_uri, { code: code, state: query.state });
+    }
+    // 付与方式が不正の場合はエラーを通知する
+    else {
+      redirect_url = buildUrl(query.redirect_uri, { error: '付与方式が不正です' });
+    }
+  }
+  // ユーザが権限委譲を拒否した場合
+  // リダイレクトURLに、エラーの旨を伝える
+  else {
+    redirect_url = buildUrl(query.redirect_uri, { error: '拒否られましたわ' });
+  }
+
+  // 確定したURLにリダイレクト
+  console.log(redirect_url);
+  res.redirect(redirect_url);
 });
 
 app.post("/token", function(req, res){
@@ -66,6 +125,10 @@ app.post("/token", function(req, res){
    * Process the request, issue an access token
    */
 
+});
+
+app.get('/', function(req, res) {
+  res.render('index', {clients: clients, authServer: authServer});
 });
 
 var buildUrl = function(base, options, hash) {
